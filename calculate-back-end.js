f9937b073a39ee62fba79bdbdf12b5258eb9dd71
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-06-22 21:52:10 -0400

    finish

--- calculate-back-end.js
+++ calculate-back-end.js
@@ -7,14 +7,16 @@ function calculate(num, num2, operator){
   num =Number(num);
   num2 =Number(num2);
 
-// // let notAdd = operator !=="+";
-// let notSub = operator !=="-";
-// let notMul = operator !=="x";
+let notAdd = operator !=="+";
+let notSub = operator !=="-";
+let notMul = operator !=="x";
 let notDiv = operator !=="/";
 let notMod = operator !=="%";
 let add = operator === "+" || operator === "plus" || operator === "added to";
 let sub = operator === "-" || operator === "minus" || operator === "subtracted from";
-let mul = operator === "x" || operator ==="X" || operator === "times" || operator === "multiplied by"
+let mul = operator === "x" || operator === "X" || operator === "times" || operator === "multiplied by"
+let div = operator === "/" || operator === "divided by"
+let mod = operator === "%" || operator === "modulus" || operator === "mod"
 
 if (add){
   return num + num2;
@@ -26,14 +28,14 @@ else if(mul) {
   return num * num2;
 
 }
-else if(operator === "/"){
+else if(div){
   return num / num2;
 }
-else if(operator === "%"){
+else if(mod){
   return num % num2 ;
 
 }
-else if( operator !=="+"|| operator !=="-"|| operator !=="x"||notDiv||notMod){
+else if( notAdd|| notSub|| notMul||notDiv||notMod){
     return "Sorry, that's not a mathematical operation!"
 }
 
